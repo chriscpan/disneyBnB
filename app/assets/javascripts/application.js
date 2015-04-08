28b0bfd00b3ed53fae9e92d58b433b2168008109
Chris <chrispy004@gmail.com>
2015-04-08 09:40:39 -0700

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,6 @@
 //= require bootstrap
 //= require underscore
 //= require backbone
-//= require disneybnb
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
