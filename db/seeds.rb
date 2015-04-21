7dbe83667ab7ab0040ecae842f1cba19e89070af
Chris <chrispy004@gmail.com>
2015-04-20 19:39:11 -0700

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,6 +50,69 @@ user5 = User.create(
   description: "Whilst presenting a charming and respectable exterior to the Sultan and the people of Agrabah, he secretly holds everyone around him in contempt and manipulates the Sultan by means of hypnotizing him with a magical snake-headed staff, which he always carries on his person. Even regarding his desiring marriage for Jasmine afterwards, it was solely so he could gain control of the throne and intended to kill her soon thereafter—though he later reneges on this in an attempt to make her his queen consort. He's traveled widely and amassed a wide knowledge of magical artifacts and legends. Until he uses Genie's powers to become a sorcerer, however, his magical abilities are limited to the use of such artifacts as he's collected, and his prowess as an accomplished alchemist."
   )
 
+user6 = User.create(
+  username: 'Bambi',
+  email: 'Bambi@Woods',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-bambi.jpg',
+  description: "Bambi is not very strongly characterized. This was one of the film’s key strengths as an environmental film, the point because Bambi---as with most of his friends---is similar to any deer in any forest. In his early youth, Bambi has the full measure of charm that many young mammals display, with his wide eyes, spindly legs, curious nature and a good cute voice. As he grows he gradually becomes more mature, but even in young adulthood he always seems a very young buck, with a graceful build and a fairly naïve, shy nature. He was nicer than his mean rival, Ronno who being jealous to poor Bambi. When he had two fawns, Geno and Gurri, he likes to be a carefree father for being happy and sweet to his children."
+)
+
+user7 = User.create(
+  username: 'Rapunzel',
+  email: 'Rapunzel@Tangled',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-rapunzel.jpg',
+  description: "Rapunzel is not a typical Disney Princess; she carries her blonde hair, which is 70 feet long, and has the magical ability to both heal and reverse aging when she sings a special Healing Incantation song. Kidnapped by Mother Gothel when she was still an infant, Rapunzel had been raised and locked away in a tall tower up until her 18th birthday, when she is 'rescued' by Flynn Rider. However, she does view Mother Gothel as her maternal mother despite being kidnapped. Rapunzel spends her time painting, cooking, candle-making, reading, brushing her hair, playing with Pascal, observing the outside world from within the tower, singing, and interacting with Mother Gothel. Being immensely fascinated by the floating lanterns that took place on her birthday every year, Rapunzel begins to develop a sense of curiosity and seeks to fulfill the dream of seeing those 'lights in the sky' one day. However, she does not overcome Mother Gothel's strict guardianship and thus has been staying in the tower, longing for the day when her life 'begins.'"
+)
+
+user8 = User.create(
+  username: 'Pascal',
+  email: 'Pascal@Tangled',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-pascal.jpg',
+  description: "Pascal is a true and loyal friend to Rapunzel. Even though he can't communicate verbally, it is clear that he is one of the driving forces in helping Rapunzel see that she needs to leave the tower. Pascal is constantly cheering her up, rooting her on, and refusing to allow her to take steps backwards. However, even though he cheers her on, he doesn't know much about the outside world either, and despite being just as excited as Rapunzel to experience it, he was originally just as afraid. Since he is a chameleon, he can change color, usually to express his mood with his colors."
+)
+
+user9 = User.create(
+  username: 'Baymax',
+  email: 'Baymax@BigHero6',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-baymax.jpeg',
+  description: "Baymax was created by the brilliant Tadashi Hamada as a healthcare providing robot nurse for the citizens of San Fransyoko and his brother, Hiro Hamada. Tadashi's goal in creating Baymax was to help improve healthcare around the world, basically wanting nothing more than to use his creation and genius for the better of humanity. To provide him with his programming, and overall personality, Baymax was given a special chip with Tadashi's inscriptions, which makes him the lovable robot he truly is. Without it, he can be conceived as an entirely different being. As programming would have, Baymax is instantly summoned by the sound of distress, and can only deactivate once his current patient states 'I am satisfied with my care.'"
+)
+
+user10 = User.create(
+  username: 'Dory',
+  email: 'Dory@Sharks',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-dory.jpg',
+  description: "She is very friendly and makes quickly makes friends with her warm hearted and fun personality. Her kindness was shown as she continued to accompany Marlin on his journey despite his lack of patience with her and his sometimes harsh comments. She developed such a close relationship with Marlin that her short term memory loss seemed to have improved, as she herself states 'Please don't leave me. No one has ever stayed with me this long and when I look at you, I remember stuff better'. The two remain close friends by the end of the film."
+)
+
+user11 = User.create(
+  username: 'Merida',
+  email: 'Merida@Brave',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-merida.jpg',
+  description: "Princess Merida is the headstrong and free-spirited 16-year-old tomboyish, willful daughter of Queen Elinor, who rules the kingdom alongside her husband, King Fergus. Queen Elinor's expectations of her daughter cause Merida to see Elinor as being distant while also causing friction between the two. Despite Elinor's desire to see Merida as a proper royal lady, Merida is an impetuous girl who wants to take control of her own destiny. Merida has honed her skill in archery, and is one of the most skilled archers ever seen. She is also skilled in sword-fighting and racing across the countryside on her horse, Angus. Despite her outgoing, forceful, headstrong and willful personality, Merida does have a playful softness of heart; particularly when it comes to her younger triplet brothers."
+)
+
+user12 = User.create(
+  username: 'Mushu',
+  email: 'Mushu@Mulan',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-mushu.jpg',
+  description: "In contrast to Mulan, Mushu is in most situations more comical, overconfident, funny and impulsive. He strives to be one of the family guardians again and selfishly decides to convince Mulan to join the army to turn her into a war hero, believing this will get him back on the 'top shelf.' However, he comes to realize how selfish he has been, admitting to Mulan that he risked her life to further his own goals. He then intends to take her home to face the consequences of his actions, but when she insists on stopping the Huns, Mushu loyally follows her, showing that underneath, he has a good heart. Mushu is rather sensitive about his size."
+)
+
+user13 = User.create(
+  username: 'Ursula',
+  email: 'Ursula@Mermaid',
+  password: 'password',
+  picture_url: 'https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/user-ursula.gif',
+  description: "Ursula is a very dark and sinister villain with a sense a humor. Ursula is very manipulative and has a talent of making great sense and making persuasive points when trying to strike a deal. Unlike many Disney Villains, most of whom care very little for their followers, Ursula puts her henchmen Flotsam and Jetsam before anything and everyone else. Ursula treats the eels like children and refer to them as 'babies' or 'poopsies'. When the eels are killed accidentally by Ursula because of Ariel, she mourns and takes all of her anger and sadness out on Ariel and attempts to kill her once and for all. She is known to fill the heads of merpeople with deceptions; she feeds Ariel with the lie (or at best half-truth) that men on the surface like silence in women. In fact, Prince Eric loved Ariel's voice, and Ursula manipulated her into giving up precisely the thing that would have let him recognize the mysterious girl who had saved him from the sea. Despite using contracts to get her way, Ursula has little regard for following her end of the bargain. This is demonstrated when immediately after Triton signed away his freedom for Ariel's safety, Ursula claims she'd have no qualms killing Ariel, despite being implied to have promised to not harm Ariel as a condition of the contract, and gleefully tries to in the final battle. The latter instance is possibly due to Ariel being the cause of Flotsam and Jetsam's deaths"
+)
 listing1 = Listing.create(
   price: 100,
   capacity: 2,
@@ -127,7 +190,7 @@ listing4 = Listing.create(
   title: 'Atlantica',
   address: '123 Atlantis',
   description: "The kingdom is located beneath the Pacific Ocean and is ruled by the sea king, King Triton. Atlantica's most famous residents are none other than King Triton's adventurous daughter Ariel, her best friend Flounder and the court composer crab Sebastian. These friends usually get Atlantica in hot water but cleans up the mess they sometimes start. The Kingdom was once ruled for a short time by the evil sea witch and former member of the royal court of King Triton, Ursula.",
-  owner_id: 5,
+  owner_id: 13,
   latitude: 37.76244,
   longitude: -122.5550
 )
@@ -221,7 +284,7 @@ listing8 = Listing.create(
   address: "123 Tangle Lane",
   description: "Rapunzel's Tower is hidden in a large rock pit by a cave. The cave leads out into the forest with its opening hidden by a curtain of plants. At the base of the tower is an alternate entrance Mother Gothel used to go in and out of the tower before she took Rapunzel and in the years that she was too young to pull her up, with a hidden trapdoor leading up into the main rooms of the Tower.\n
 Within the tower and over the years, Rapunzel has painted a series of murals. The tower has several rooms including bedrooms for both Rapunzel and Mother Gothel, a central room which would be the primary location of Rapunzel's gallery, and a staircase leading to an upstairs room.",
-  owner_id: 1,
+  owner_id: 8,
   latitude: 37.828015,
   longitude: -122.374920
 )
@@ -237,3 +300,142 @@ Within the tower and over the years, Rapunzel has painted a series of murals. Th
     listing_id: 8,
     image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/rapunzels-room-2.jpg"
   )
+
+listing9 = Listing.create(
+  price: 300,
+  capacity: 3,
+  title: "The Forest",
+  address: "123 Deer Lane",
+  description: "The forest is where Bambi and other characters live. It is the main setting of Bambi and Bambi II. It is also where Bambi, Flower and Thumper raise their families.",
+  owner_id: 6,
+  latitude: 37.855617,
+  longitude: -122.544638
+)
+  image901 = Image.create(
+    listing_id: 9,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/forest-room-1.jpg"
+  )
+  image902 = Image.create(
+    listing_id: 9,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/forest-room-2.jpg"
+  )
+  image903 = Image.create(
+    listing_id: 9,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/forest-room-3.jpg"
+  )
+
+listing10 = Listing.create(
+  price: 600,
+  capacity: 2,
+  title: "Corona Castle",
+  address: "123 Lantern Way",
+  description: "The castle was one of the last buildings to be built on the island. Every year on Rapunzel's birthday, the entire kingdom sends floating lanterns into the sky. The King and Queen release the first lantern at the castle. 18 years later, Flynn Rider and The Stabbington Brothers break into the castle to steal the crown of the lost princess. When Rapunzel finally meet her true parents, they meet in the castle.",
+  owner_id: 7,
+  latitude: 37.863207,
+  longitude: -122.430655
+)
+  image1001 = Image.create(
+    listing_id: 10,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/corona-room-1.jpg"
+  )
+  image1002 = Image.create(
+    listing_id: 10,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/corona-room-2.jpg"
+  )
+  image1003 = Image.create(
+    listing_id: 10,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/corona-room-3.jpg"
+  )
+
+listing11 = Listing.create(
+  price: 450,
+  capacity: 3,
+  title: "San Fransokyo",
+  address: "123 Robot Lane",
+  description: "The layout of the city is a futuristic hybrid of artistic and technological architecture, and a blend of both San Francisco and Tokyo architectural styles. Built on the coast of the Pacific Ocean, the city is designed to withstand the risk of earthquakes. The city is also a center for vast cultures besides the dominant Eastern and Western themes. However, behind the bright aesthetic is the underworld of illegal bot fighting, often located in the dark, grungy alleys of the city. Many of the buildings in the city were constructed by Alistair Krei and his company, Krei Tech.",
+  owner_id: 9,
+  latitude: 37.786054,
+  longitude: -122.429904
+)
+  image1101 = Image.create(
+    listing_id: 11,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/san-fransokyo-room-1.jpg"
+  )
+  image1102 = Image.create(
+    listing_id: 11,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/san-fransokyo-room-3.jpg"
+  )
+  image1103 = Image.create(
+    listing_id: 11,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/san-fransokyo-room02.jpg"
+  )
+
+listing12 = Listing.create(
+  price: 75,
+  capacity: 4,
+  title: "Great Barrier Reef",
+  address: "123 Jellyfish Circle",
+  description: "Two ocellaris clownfish, Marlin and his wife Coral, admire their new home in the Great Barrier Reef and their clutch of eggs when a barracuda attacks, knocking Marlin unconscious. He wakes up to find Coral and all but one of the eggs have disappeared. Marlin names this last egg Nemo, a name that Coral liked.",
+  owner_id: 10,
+  latitude: 37.717783,
+  longitude: -122.312981
+)
+  image1201 = Image.create(
+    listing_id: 12,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/nemo-room-1.jpg"
+  )
+  image1202 = Image.create(
+    listing_id: 12,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/nemo-room-2.jpg"
+  )
+  image1203 = Image.create(
+    listing_id: 12,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/nemo-room-3.jpg"
+  )
+
+listing13 = Listing.create(
+  price: 250,
+  capacity: 5,
+  title: "Forest of Mor'du",
+  address: "123 Bear St",
+  description: "Mor'du was once a human prince, the eldest of four brothers, all the sons of a wise king beloved by his people. One dark autumn, the king grew ill. On his death-bed, the king requested the kingdom be divided among his four sons, so together they could be the pillars that would hold it together and maintain peace. Alas, his firstborn son felt that, as the eldest and the technical heir, he alone had the right to rule the whole kingdom and a seed of selfishness grew in him like a poison. To his brothers, he claimed his right to rule and demanded their obedience, shattering the bonds of their brotherhood by breaking the family stone.",
+  owner_id: 11,
+  latitude: 37.886513,
+  longitude: -122.586953
+)
+  image1201 = Image.create(
+    listing_id: 13,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/mordu-forest-room-1.jpg"
+  )
+  image1202 = Image.create(
+    listing_id: 13,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/mordu-forest-room-2.jpg"
+  )
+  image1203 = Image.create(
+    listing_id: 13,
+    image_url: "https://s3-us-west-1.amazonaws.com/disneybnb/disneybnb-images/mordu-forest-room-3.jpg"
+  )
+
+# listing14 = Listing.create(
+#   price: 150,
+#   capacity: 3,
+#   title: "Imperial City",
+#   address: "123 Dragon Way",
+#   description: "The city serves as the capital of China and is the location of the Emperor's Palace, also known as the Forbidden City. According to the film, one of the quickest ways to the Imperial City is to travel through a small mountain pass.
+# In the film, the Chinese people hold a celebration in the Imperial City to celebrate the defeat of the Huns. However, a small group of Huns had survived and secretly entered the city by hiding in a Chinese dragon puppet."
+#   owner_id: 12,
+#   latitude: 37.792993,
+#   longitude: -122.406649
+# )
+#   image1201 = Image.create(
+#     listing_id: 14,
+#     image_url: ""
+#   )
+#   image1202 = Image.create(
+#     listing_id: 14,
+#     image_url: ""
+#   )
+#   image1203 = Image.create(
+#     listing_id: 14,
+#     image_url: ""
+#   )
